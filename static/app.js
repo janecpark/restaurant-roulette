b2d615de9aff86676406456825fea0dd722f642e
janecpark <janecpark@users.noreply.github.com>
2020-05-11 22:33:16 -0700

    fix

--- static/app.js
+++ static/app.js
@@ -1,5 +1,5 @@
-const BASE_URL ='http://127.0.0.1:5000'
-// const BASE_URL = 'https://restaurant-roulette-v1.herokuapp.com/'
+// const BASE_URL ='http://127.0.0.1:5000'
+const BASE_URL = 'https://restaurant-roulette-v1.herokuapp.com/'
 let savedRes = []
 let clickedID = []
 let username = []
